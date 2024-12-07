75e6a00576c2b872818373c403dcf6537aa87187
MrDrups <125366653+MrDrups@users.noreply.github.com>
2024-12-07 10:46:14 +0300

    Delete kurs directory

--- kurs/src/main/java/Licences/repository/LicenseLogRepository.java
+++
@@ -1,9 +0,0 @@
-package Licences.repository;
-
-import Licences.model.LicenseLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LicenseLogRepository extends JpaRepository<LicenseLog, Long> {
-}
--- kurs/src/main/java/Licences/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package Licences.repository;
-
-import Licences.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
